c7bb80d845f565bed8f6f06af4b84afeb64d9bfc
Александр <hyperpositron@gmail.com>
2023-03-11 09:19:32 +0200

    Конструктор для Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,6 +2,11 @@ public class Vehicle {
     public String modelName;
     public int whealsCount;
 
+    public Vehicle(String modelName, int whealsCount) {
+        this.modelName = modelName;
+        this.whealsCount = whealsCount;
+    }
+
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
