bb0c23fc6f8a494500afcfc52cdd99dc47f65db9
Александр <hyperpositron@gmail.com>
2023-03-11 10:42:05 +0200

    Добавил инкапсуляцию!

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,6 +1,6 @@
 public abstract class Vehicle {
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
 
     public Vehicle(String modelName, int wheelsCount) {
         this.modelName = modelName;
@@ -9,5 +9,21 @@ public abstract class Vehicle {
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
     }
 }
