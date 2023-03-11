f0a102832fa2bea1a45e7756df604b62289dde3f
Александр <hyperpositron@gmail.com>
2023-03-11 09:23:10 +0200

    Конструктор для Bicycle.java Super

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,8 +1,11 @@
 public class Bicycle extends Vehicle {
-//    public String modelName;
+    //    public String modelName;
 //    public int whealsCount;
 //
 //    public void updateTyre() {
 //        System.out.println("Меняем покрышку");
 //    }
+    public Bicycle(String name, int wheal) {
+        super(name, wheal);
+    }
 }
